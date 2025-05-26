dee70c1a26d901ddddeaf96f77a10d5c92233cc6
Vin <m.vin.321@outlook.com>
2025-05-26 17:02:19 +0800

    update username references - comment/notes only

--- exec-lock.sh
+++ exec-lock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ExecLock - Locks script when already running
-# Copyright (C) 2025 https://github.com/mvin321
+# Copyright (C) 2025 https://github.com/SlashUsrVin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
